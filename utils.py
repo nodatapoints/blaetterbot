c6201292841ab3e8c341550f913e078b6875ed7f
nodatapoints <nodatapoints@gmail.com>
2019-10-19 13:50:37 +0200

    add subscription file context

--- utils.py
+++ utils.py
@@ -1,11 +1,15 @@
 from contextlib import contextmanager
+from pathlib import Path
 import logging
-
-logger = logging.getLogger('bot')
 
 import yaml
 
 from mirror import Mirror
+
+__all__ = 'config', 'subscription_file'
+
+logger = logging.getLogger('bot')
+subscribers_dir = Path('subscribers')
 
 @contextmanager
 def config(filename):
@@ -22,3 +26,23 @@ def config(filename):
         )
         fobj.truncate()
 
+@contextmanager
+def subscription_file(userid):
+    user_file = subscribers_dir / userid
+    if not user_file.exists():
+        user_file.touch()
+        logger.debug(f'Created {user_file.as_posix()}')
+
+    fobj =  user_file.open('r+')
+
+    try:
+        subscriptions = set(map(str.strip, fobj.readlines()))
+
+        yield subscriptions
+
+        fobj.seek(0)
+        fobj.write('\n'.join(subscriptions))
+        fobj.truncate()
+
+    finally:
+        fobj.close()
