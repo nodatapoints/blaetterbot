78aea431800664b7a054b73bbfe0f495ada66636
nodatapoints <nodatapoints@gmail.com>
2019-10-21 18:51:14 +0200

    make bot.py failsafe

--- bot.py
+++ bot.py
@@ -14,17 +14,21 @@ log.info('Starting ...')
 
 bot = Bot(config['token'])
 for mirror in fetchers(config):
-    pdf = mirror.fetch()
-    if not pdf:
-        log.debug('skipping')
-        continue
+    try:
+        pdf = mirror.fetch()
+        if not pdf:
+            log.debug('skipping')
+            continue
 
-    for uid in subscribed_users(mirror.lecture_id):
-        bot.send_document(
-            chat_id=uid,
-            document=mirror.process_pdf(pdf),
-            filename=mirror.filename
-        )
+        for uid in subscribed_users(mirror.lecture_id):
+            bot.send_document(
+                chat_id=uid,
+                document=mirror.process_pdf(pdf),
+                filename=mirror.filename
+            )
 
-    mirror.increment()
-    log.info(f'uploaded {mirror.filename}')
+        mirror.increment()
+        log.info(f'uploaded {mirror.filename}')
+
+    except Exception as e:
+        log.exception(f'failed {mirror.lecture_id}')
